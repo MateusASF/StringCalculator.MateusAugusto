43864d30ed9285859b121a29bf0646c80888583f
Mateus Augusto dos Santos Fonseca <mateus.asfonseca@gmail.com>
2022-09-28 21:32:43 -0300

    Adicionar arquivos de projeto.

---
+++ StringCalculator/StringCalculators.cs
@@ -0,0 +1,25 @@
+﻿namespace StringCalculator.Domain
+{
+    public class StringCalculators
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+                return 0;
+
+            var parts = numbers.Split(",");
+
+            var exceedsCount = parts.Length > 2;
+            var consecutiveCommands = parts.Any(x => x == "");
+            var nomNumbers = parts.Any(x => !int.TryParse(x, out _));
+
+
+            if (exceedsCount || consecutiveCommands || nomNumbers)
+                throw new ArgumentException(nameof(numbers));
+
+            var sum = parts.Sum(Convert.ToInt32);
+
+            return sum;
+        }
+    }
+}
---
+++ TestProject1/StringCalculatorUnitTests.cs
@@ -0,0 +1,55 @@
+using FluentAssertions;
+using StringCalculator.Domain;
+
+namespace StringCalculator.UnitTests
+{
+    public class StringCalculatorUnitTests
+    {
+        [Fact]
+        public void Add_WhenEmptySting_ShouldReturnZero()
+        {
+            var sut = new StringCalculators();
+            var result = sut.Add("");
+            result.Should().Be(0);
+        }
+
+        [Fact]
+        public void Add_WhenStringIsNull_ShouldReturnZero()
+        {
+            var sut = new StringCalculators();
+
+            Action add = () => sut.Add("1,2,3");
+
+            add.Should().Throw<ArgumentException>().WithMessage("*numbers*");
+        }
+
+        [Fact]
+        public void Add_WhenConsecutiveCommas_ShouldThrowException()
+        {
+            var sut = new StringCalculators();
+
+            Action add = () => sut.Add("1,,3");
+
+            add.Should().Throw<ArgumentException>().WithMessage("*numbers*");
+        }
+
+
+        [Fact]
+        public void Add_WhenContainsNomNumber_ShouldThrowArgumentExecption()
+        {
+            var sut = new StringCalculators();
+
+            Action add = () => sut.Add("1,a");
+
+            add.Should().Throw<ArgumentException>().WithMessage("*numbers*");
+        }
+
+        public void Add_WhenValidInput_ReturnsSum()
+        {
+            var sut = new StringCalculators();
+            var result = sut.Add("1,3");
+
+            result.Should().Be(4);
+        }
+    }
+}
